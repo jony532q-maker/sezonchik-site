13022e6728f6d2b1f8f9702ee35f2bebda483fde
jony532q-maker <jony532q@gmail.com>
2026-07-13 17:39:08 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sezonchik-v4';
+const CACHE='sezonchik-v3-final';
 const CORE=['./','./index.html','./style.css','./script.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
